3ef6edfda2f537ef0065ddaaa0905794cf4fea88
hellxz <hellxz001@foxmail.com>
2019-11-28 18:36:23 +0800

    修改pom.xml配置，测试demo中有的不使用spring security

---
+++ authorization-code-client/src/main/java/com/github/hellxz/oauth2/AuthorizationCodeClientApp.java
@@ -0,0 +1,11 @@
+package com.github.hellxz.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationCodeClientApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationCodeClientApp.class, args);
+    }
+}
